abbc4845fb8560bbcdc5aebd240135a1fff28928
Taras Rushchak <taraskhust@gmail.com>
2019-02-15 23:22:16 +0200

    add class EmailParser for chek email

--- Урок 2/js-lesson2-src/es6/lesson2/hw.js
+++ Урок 2/js-lesson2-src/es6/lesson2/hw.js
@@ -1,23 +1,27 @@
 /* global Proxy */
-function watchObj(node, callback){
-    /* return new Proxy(node, {...}) */
-    return node;
+'use strict';
+
+function watchObj(node, callback) {
+	/* return new Proxy(node, {...}) */
+	return node;
 }
 
-class EmailParser{
-    constructor(email){
-        this.email = email;
-    }
+class EmailParser {
+	constructor(email) {
+		this.email = email;
+	}
 
 	get isCorrect() {
-      return true || false
-  }
-  get name() {
-    this.email.
-  }
-  get domain() {
+		return (/\S+@\S+\.\S+/).test(this.email) ? true : null
+	}
 
-  }
+	get name() {
+		return this.isCorrect ? true : null
+	}
+
+	get domain() {
+		return this.isCorrect ? true : null
+	}
 }
 
 export {watchObj, EmailParser};
